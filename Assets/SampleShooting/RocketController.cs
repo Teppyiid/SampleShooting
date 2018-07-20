a0afd9437571b50197f4bd8bb09e575c138d627f
Teppyiid <vzgundam.kai@gmail.com>
2018-07-20 11:24:27 +0900

    first commit

---
+++ Assets/SampleShooting/RocketController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            transform.Translate(-0.1f, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.Translate(0.1f, 0, 0);
+        }
+    }
+}
