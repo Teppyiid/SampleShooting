41027c91cac0f76148515893b5fc62e009d2b6b0
Teppyiid <vzgundam.kai@gmail.com>
2018-07-23 16:28:46 +0900

    third done

--- Assets/SampleShooting/RocketController.cs
+++ Assets/SampleShooting/RocketController.cs
@@ -3,13 +3,18 @@ using UnityEngine;
 
 public class RocketController : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update () {
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update () {
         if (Input.GetKey(KeyCode.LeftArrow)) {
             transform.Translate(-0.1f, 0, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow)) {
             transform.Translate(0.1f, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Instantiate(bulletPrefab, transform.position, Quaternion.identity);
         }
     }
 }
